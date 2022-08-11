e16843f0ce1a5b6b81998793d821a25cdfef155a
Nada Feteiha <nada.feteiha@gmail.com>
2022-08-11 10:42:32 -0700

    refactor

--- app/src/main/java/com/moataz/weather/data/request/NetworkResult.kt
+++
@@ -1,7 +0,0 @@
-package com.moataz.weather.data.request
-
-sealed class NetworkResult<T>{
-    class Success<T>(val transferredData: T) : NetworkResult<T>()
-    class Failure<T>(message: String) : NetworkResult<T>()
-    class Loading<T> : NetworkResult<T>()
-}
